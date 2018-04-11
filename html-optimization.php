c1cb529af23b6415e48b916b71a9bee78db58d80
o10n-x <info@optimization.team>
2018-04-11 13:51:30 +0200

    core update

--- html-optimization.php
+++ html-optimization.php
@@ -15,6 +15,7 @@ namespace O10n;
  * Version:           0.0.10
  * Author:            Optimization.Team
  * Author URI:        https://optimization.team/
+ * GitHub Plugin URI: https://github.com/o10n-x/wordpress-html-optimization
  * Text Domain:       o10n
  * Domain Path:       /languages
  */
